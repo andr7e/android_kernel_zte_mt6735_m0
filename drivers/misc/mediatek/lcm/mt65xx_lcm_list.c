aa83d122f24e25730bd28b1c2dd0f0be027c00f4
andre <andr13e@gmail.com>
2017-07-27 10:50:46 +0300

    add lcm otm1287a_720p_dsi

--- drivers/misc/mediatek/lcm/mt65xx_lcm_list.c
+++ drivers/misc/mediatek/lcm/mt65xx_lcm_list.c
@@ -60,6 +60,10 @@ LCM_DRIVER *lcm_driver_list[] = {
 	&otm1287a_720p_dsi_dj_lcm_drv,
 #endif
 
+#if defined(OTM1287A_720P_DSI)
+	&otm1287a_720p_dsi_lcm_drv,
+#endif
+
 #ifdef AEON_FACTORY_MODULE
 #if defined(AEON_FACTORY_LCM)  //Don't modify !!
 	&aeon_factory_lcm_lcm_drv,
